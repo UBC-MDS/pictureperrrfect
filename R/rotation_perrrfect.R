ca8da257f36984e57c71600098420527f02ff28e
Dustin Burnham <dusty736@student.ubc.ca>
2021-03-05 18:34:46 -0800

    Add roxygen2 documentation for rotation function

---
+++ R/rotation_perrrfect.R
@@ -0,0 +1,13 @@
+#' rotation_perrrfect rotates an image in matrix form by
+#' either 90, 180, 270 degrees.
+#'
+#' @param image Matrix of pixel values to be rotated
+#' @param n_rot Integer number of 90-degree rotations to return
+#'
+#' @return Matrix of rotated pixel values
+#'
+#' @examples
+#' rotation_perrrfect(matrix(c(1, 2, 3, 11, 12, 13), nrow = 2, ncol = 3), 1)
+rotation_perrrfect <- function(image, n_rot) {
+
+}

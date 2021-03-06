82c0c72660b0dbf7ddee003f022ea0ff73661fb6
Kangbo Lu <kblu@ucdavis.edu>
2021-03-05 19:33:45 -0800

    Update comments for get_property_perrrfect()

---
+++ R/get_property_perrrfect.R
@@ -0,0 +1,11 @@
+#' This function will get the image property of a image.
+#' 
+#' @param image matrix to obtain image information
+#'
+#' @return a list of image dimensions, total pixles, the mean and median of each channel 
+#'
+#' @examples
+#' get_property_perrrfect(matrix(c(1, 2, 3, 11, 12, 13)))
+get_property_perrrfect <- function(image) {
+
+}

6bb9b6a4877d642a10f1c600c390cb4736cbc0c9
KangboLu <kblu@ucdavis.edu>
2021-03-12 20:00:27 -0800

    add test image for get_property

--- R/get_property_perrrfect.R
+++ R/get_property_perrrfect.R
@@ -1,8 +1,8 @@
 #' This function will get the image property of a image.
-#' 
+#'
 #' @param image matrix to obtain image information
 #'
-#' @return a list of image dimensions, total pixles, the mean and median of each channel 
+#' @return a list of image dimensions, total pixles, the mean and median of each channel
 #'
 #' @examples
 #' get_property_perrrfect(matrix(c(1, 2, 3, 11, 12, 13)))

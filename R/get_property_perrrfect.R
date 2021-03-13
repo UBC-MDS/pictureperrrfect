4e301266e5cacdb42cae73849c285835d0da5b2b
KangboLu <kblu@ucdavis.edu>
2021-03-12 20:43:39 -0800

    add example to get_property

--- R/get_property_perrrfect.R
+++ R/get_property_perrrfect.R
@@ -7,7 +7,25 @@ library(imager)
 #' @return  a list of image dimensions, pixels, and each channel's mean and median
 #'
 #' @examples
-#' get_property_perrrfect(image)
+#' get_property_perrrfect(image) # all black image
+#' $width
+#' [1] 150
+#'
+#' $height
+#' [1] 200
+#'
+#' $pixels
+#' [1] 30000
+#'
+#' $R
+#' [1] 0 0
+#'
+#' $G
+#' [1] 0 0
+#'
+#' $B
+#' [1] 0 0
+
 get_property_perrrfect <- function(image) {
     # error handling
     if (!imager::is.cimg(image)) {

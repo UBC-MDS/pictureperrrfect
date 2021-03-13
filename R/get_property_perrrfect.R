cbf39f57366c0608094ce867b93fb31ccb77f93a
KangboLu <kblu@ucdavis.edu>
2021-03-12 20:20:27 -0800

    add stable get property function

--- R/get_property_perrrfect.R
+++ R/get_property_perrrfect.R
@@ -1,11 +1,35 @@
+library(imager)
+
 #' This function will get the image property of a image.
 #'
-#' @param image matrix to obtain image information
+#' @param image imager::cimg image to obtain image information
 #'
-#' @return a list of image dimensions, total pixles, the mean and median of each channel
+#' @return  a list of image dimensions, pixels, and each channel's mean and median
 #'
 #' @examples
-#' get_property_perrrfect(matrix(c(1, 2, 3, 11, 12, 13)))
+#' get_property_perrrfect(image)
 get_property_perrrfect <- function(image) {
+    # error handling
+    if (!imager::is.cimg(image)) {
+        stop("Cannot process a non-cimg object")
+    }
 
+    # convert image to data frame for manipulation
+    image <- as.data.frame(image)
+
+    # calculate to extract insights from image
+    width <- max(image$x)
+    height <- max(image$y)
+    red_channel <- image[image$cc == 1,]$value
+    green_channel <- image[image$cc == 2,]$value
+    blue_channel <- image[image$cc == 3,]$value
+
+    # output image statistics
+    list("width" = width,
+         "height" = height,
+         "pixels" = width * height,
+         "R" = c(mean(red_channel), median(red_channel)),
+         "G" = c(mean(green_channel), median(green_channel)),
+         "B" = c(mean(blue_channel), median(blue_channel))
+    )
 }
